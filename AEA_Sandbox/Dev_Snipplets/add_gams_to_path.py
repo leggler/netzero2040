dbcc30dae5bf5fdc6ed7ba41dde7a8e9909d8831
Lukas <lukas.zwieb@energyagency.at>
2022-01-25 11:18:53 +0100

    add GAMS databases handling I

--- AEA_Sandbox/Dev_Snipplets/add_gams_to_path.py
+++ AEA_Sandbox/Dev_Snipplets/add_gams_to_path.py
@@ -1,54 +1,67 @@
 import gams
-
+from pathlib import Path
+import os
+from gams.workspace import GamsException
 # print(help(gams))
 
-ws = gams.GamsWorkspace()
-path = r"C:\Users\lzw\PycharmProjects\netzero2040\zwiebs_test_suite\BasicModel.gdx"
-db2 = ws.add_database_from_gdx(path)
+def load_gdx_from_path(path:str):
+    """
+    return a the gams db based on the a realative path from project root
+    :param path: relative path from content root (e.g. "AEA_Sandbox/OeM/OEM-Jan-Base.gdx")
+    :return: gams gdx db file
+    """
+    project_root = Path(__file__).parent.parent.parent
+    path = os.path.join(project_root, "AEA_Sandbox/OeM/OEM-Jan-Base.gdx")
+    ws = gams.GamsWorkspace()
+    gdx_db = ws.add_database_from_gdx(path)
+    return gdx_db
 
-plants = ["Seattle", "San-Diego"]
-markets = ["New-York", "Chicago", "Topeka"]
-capacity = {"Seattle": 350.0, "San-Diego": 600.0}
-demand = {"New-York": 325.0, "Chicago": 300.0, "Topeka": 275.0}
-distance = {("Seattle", "New-York"): 2.5,
-            ("Seattle", "Chicago"): 1.7,
-            ("Seattle", "Topeka"): 1.8,
-            ("San-Diego", "New-York"): 2.5,
-            ("San-Diego", "Chicago"): 1.8,
-            ("San-Diego", "Topeka"): 1.4
-            }
+"VAR_FLO; PRC: EGRdELC00, COM: ELCELC; TS; WN,WD,TN, TD, SN, SD"
 
-db = ws. add_database()
-
-
-i = db.add_set("i", 1, "canning plants")
-
-"""for p in plants:
-    i.add_record(p)
-
-for x in db:
-    print(x.get_name())
-
-a = db.add_parameter_dc("a", [i], "capacity of plant i in cases")
-for p in plants:
-    a.add_record(p).value = capacity[p]
-"""
+parameter = [
+    "VAR_FLO",
+    "PRC",
+    "COM",
+    "TS",
+    "WN",
+    "WD",
+    "TN",
+    "SN",
+    "SD"]
+def list_all_entities_of_db(db):
+    for x in db:
+        print(x.get_name(), x.text)
 
 
-for x in db2:
-    if "PAR_" == x.get_name()[:4]:
 
-        #print(x.get_name(), x.text)
-        pass
-print(db2["PAR_FLO"].text, db2["PAR_FLO"].get_dimension(), db2["PAR_FLO"].domains_as_strings,db2["PAR_FLO"].number_records)
-print(db2["PAR_FLO"].__dir__())
-#print(help(db2["PAR_FLO"]))
-print(db2["PAR_FLO"].number_records)
-"""
-for x in db2["PAR_FLO"]:
-    print(x.value)
-    print(x.keys)
-    #print(help(x))
-    break
-"""
-# print(ws.__dir__())
+def read_entity(entity_name):
+    try:
+        print("\t" + gdx_db[entity_name].text)
+        print("\t" + str(gdx_db[entity_name].number_records))
+    except GamsException as e:
+        print(e)
+
+###### HELPERS #######
+#     # print(gdx_db["PAR_FLO"].text, gdx_db["PAR_FLO"].get_dimension(), gdx_db["PAR_FLO"].domains_as_strings,gdx_db["PAR_FLO"].number_records)
+#     # print(gdx_db["PAR_FLO"].__dir__())
+#     #print(help(db2["PAR_FLO"]))
+# for x in db2["PAR_FLO"]:
+#     print(x.value)
+#     print(x.keys)
+#     #print(help(x))
+#     break
+# [print(x) for x in gdx_db.__dir__()]
+
+
+if __name__ == "__main__":
+    db_path = "AEA_Sandbox/OeM/OEM-Jan-Base.gdx"
+    gdx_db = load_gdx_from_path(db_path)
+    # list all entities
+    list_all_entities_of_db(gdx_db)
+
+    #look for entities of interest
+    for entity in parameter:
+        print(entity)
+        read_entity(entity_name=entity)
+        print()
+    print(help(gdx_db))
