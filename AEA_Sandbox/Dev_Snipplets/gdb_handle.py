741bb6197e4666258a09fc54f4b5f65f77cf6ba1
Lukas <lukas.zwieb@energyagency.at>
2022-01-26 16:53:53 +0100

    Increased Readability of gdp_handle.generate_pandas_dataframe

--- AEA_Sandbox/Dev_Snipplets/gdb_handle.py
+++ AEA_Sandbox/Dev_Snipplets/gdb_handle.py
@@ -1,13 +1,15 @@
 import gams
 from pathlib import Path
 import os
-
+import pyam
 import pandas as pd
 
 from gams.workspace import GamsException
+
+
 # print(help(gams))
 
-def load_gdx_from_path(path:str):
+def load_gdx_from_path(path: str):
     """
     return a the gams db based on the a realative path from project root
     :param path: relative path from content root (e.g. "AEA_Sandbox/OeM/OEM-Jan-Base.gdx")
@@ -21,6 +23,7 @@ def load_gdx_from_path(path:str):
     gdx_db = ws.add_database_from_gdx(path)
     return gdx_db
 
+
 # Note from Martin:
 "VAR_FLO; PRC: EGRDELC00, COM: ELCELC; TS; WN,WD,TN, TD, SN, SD"
 
@@ -29,19 +32,20 @@ parameter = [
     "PRC",
     "COM",
     "TS",
-        "WN",   # Winter Night
-        "WD",   # Winter Day
-        "TN",   # Transition Night
-        "TD",   # Transition Day
-        "SN",   # Summer Night
-        "SD",   # Summer Day
+    "WN",  # Winter Night
+    "WD",  # Winter Day
+    "TN",  # Transition Night
+    "TD",  # Transition Day
+    "SN",  # Summer Night
+    "SD",  # Summer Day
     #
     "R",
     "ALLYEAR",
     "P",
     "C",
     "S"
-     ]
+]
+
 
 def list_all_entities_of_db(db):
     """
@@ -61,6 +65,7 @@ def read_entity(entity_name):
     except GamsException as e:
         print(e)
 
+
 ###### HELPERS #######
 #     # print(gdx_db["PAR_FLO"].text, gdx_db["PAR_FLO"].get_dimension(), gdx_db["PAR_FLO"].domains_as_strings,gdx_db["PAR_FLO"].number_records)
 #     # print(gdx_db["PAR_FLO"].__dir__())
@@ -76,7 +81,7 @@ def read_entity(entity_name):
 def easy_filter_by_xx(gdx_db):
     domains = gdx_db["VAR_FLO"]
     for domain in domains:
-        #print(domain)
+        # print(domain)
         if "EGRDELC00" == domain.keys[3].upper():
             print("\t", domain)
             # print(domain._symbol.__dict__)
@@ -84,98 +89,82 @@ def easy_filter_by_xx(gdx_db):
         if "ELCELC" == domain.keys[3].upper():
             print("\t", domain)
 
-def generate_dataframe():
+
+def generate_pandas_dataframe(symbol: str, commodity_filter: str, process_filter: str):
+    """
+
+    :param symbol:
+    :param commodity_filter:
+    :param process_filter:
+    :return:
+    """
+
     regions = []
     years = []
     processes = []
     commodities = []
     time_scales = []
     levels = []
-    model_names = []
     variables = []
     units = []
+
     for x in gdx_db["VAR_FLO"]:
-        # print(x.keys)
-        REG  = x.key(0)
-        YEAR = x.key(1)
-        P = x.key(3)
-        C = x.key(4)
-        S = x.key(5)
-        level = x.level
+        process = x.key(3)
+        commodity = x.key(4)
+        # Test if the entry confirms(?) the filter
+        if commodity == commodity_filter and process == process_filter:
+            region = x.key(0)
+            year = x.key(1)
+            timescale = x.key(5)
+            level = x.level
 
-        if C == "ELCELC" and P == "EGRDELC00":
-
-            regions.append(REG)
-            years.append(YEAR)
-            processes.append(P)
-            commodities.append(C)
-            time_scales.append(S)
+            regions.append(region)
+            years.append(year)
+            processes.append(process)
+            commodities.append(commodity)
+            time_scales.append(timescale)
             levels.append(level)
 
-            #Todo get Variable name
-            variables.append(f"NONE|Electricity|{P}")
-            units .append("GWH")
+            # Todo get Variable name
+            variables.append(f"NONE|Electricity|{process}")
+            units.append("GJ")
 
     df = pd.DataFrame({
-        "Region": regions,
-        "Variable": variables,
-        "Unit": units,
-        "Year": years,
-        "Value":levels,
-        "time_scale":time_scales
+        "region": regions,
+        "variable": variables,
+        "unit": units,
+        "year": years,
+        "value": levels,
+        "time_scale": time_scales
     })
 
-    df["Model"] = "OEModel"
-    df["Scenario"] = "BasicModel"
     return df
+
 
 if __name__ == "__main__":
     db_path = "AEA_Sandbox/OeM/OEM-Jan-Base.gdx"
     gdx_db = load_gdx_from_path(db_path)
-    # PRC ~ processes
-    # C ~ commodities
-    # easy_filter_by_xx(gdx_db)
-    # print(gdx_db["VAR_FLO"].get_domains_as_strings())
-    # print(dir(gdx_db["VAR_FLO"]))
-    # print(gdx_db["VAR_FLO"])
-
-
-    print("Länge der DB:", len(gdx_db["VAR_FLO"]))
-    domains = gdx_db["VAR_FLO"].get_domains()
-    for domain in domains:
-        print(domain.text)
 
     # switch this on to retrigger generate df otherwise load from pickle
-    if False:
-        df = generate_dataframe()
-
+    # do this if you changed something in generate_pandas_dataframe
+    reload = False
+    if reload:
+        df = generate_pandas_dataframe(symbol="VAR_FLO", process_filter="EGRDELC00", commodity_filter="ELCELC")
         df.to_pickle("VAR_FLO_dev.pickle")
+    else:
+        print("WARING: Reload gdx_db was set to:", reload)
 
     df = pd.read_pickle("VAR_FLO_dev.pickle")
+    df["model"] = "OeM"
+    df["scenario"] = "OEM-Jan-Base"
     print(df)
-    df_sum = df.groupby(["Model", "Scenario", "Region", "Variable", "Unit", "Year"]).sum()
+    df_sum = df.groupby(["model", "scenario", "region", "variable", "unit", "year"]).sum()
     print(df_sum.reset_index())
-    df_piv = pd.pivot_table(data=df_sum, columns=["Year"],values="Value", index = ["Model", "Scenario", "Region", "Variable", "Unit"] )
+    df_piv = pd.pivot_table(data=df_sum, columns=["year"], values="value",
+                            index=["model", "scenario", "region", "variable", "unit"])
     print(df_piv)
     df_piv.to_csv("ELCELC_export_DE.csv", sep=";", decimal=",")
     df_piv.to_csv("ELCELC_export_EN.csv")
-    quit()
 
-    # list all entities
-    # list_all_entities_of_db(gdx_db)
 
-    #look for entities of interest
-    for entity in parameter:
-        print(entity)
-        read_entity(entity_name=entity)
-        print()
-
-def get_variable_name(oem_name):
-    if oem_name == "ealskjf":
-        netzero_name = "Electricity"
-    else:
-        raise ValueError
-    return netzero_name
-
- #   [{"model":"", "scenaro": "", "region":"", "variable":"", "unit":"", "values" [}]}]
-
+    # print(pyam.IamDataFrame(df_sum))
