56eecfb590324a2bbcee721adf399da89468db43
LZW <lukas.zwieb@energyagency.at>
2022-06-21 13:04:35 +0200

    (~) move git ignore

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="netzero2040",
+    version="0.1.0",
+    author="",
+    author_email="",
+    package_dir={"": "netzero2040"},
+    # setup_requires=["black", "coloredlogs", "sphinx"],
+    tests_require=[],
+    description="changeme",
+    install_requires=[],
+)
